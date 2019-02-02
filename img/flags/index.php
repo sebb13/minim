a8314870ba88b7f2e9f29f25e8d551dd477bbd15
Seb Boulard <boulard.seb@gmail.com>
2019-02-02 12:54:36 +0100

    move flags images for lang switcher

---
+++ img/flags/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<title>RIEN A VOIR ICI !!!</title>
+		<style type="text/css">
+		<!--
+		.Style1 {
+			font-family: Arial, Helvetica, sans-serif;
+			font-weight: bold;
+			color: #FF0000;
+			font-size: xx-large;
+			font-style: italic;
+		}
+		-->
+		</style>
+	</head>
+	<body>
+		<div align="center" class="Style1">RIEN A VOIR ICI !!!</div>
+	</body>
+</html>
